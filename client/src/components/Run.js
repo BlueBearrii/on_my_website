acd1bebfc3a8c077bc274b2af6ac5c3836419e07
bluebearrii <thananan.math.acs@gmail.com>
2020-10-28 22:30:30 +0700

    Refactor rocket code

--- client/src/components/Run.js
+++ client/src/components/Run.js
@@ -7,40 +7,90 @@ export default class Run extends Component {
     super();
     this.state = {
       rocketPosition: base,
+      fireState: false,
     };
   }
 
   render() {
-    const moveRocket = () => {
+    //
+    // ────────────────────────────────────────────────────── I ──────────
+    //   :::::: M O V E   U P & DOWN : :  :   :    :     :        :          :
+    // ────────────────────────────────────────────────────────────────
+    //
+    const moveRocket = (_control) => {
+      console.log(_control);
+
+      // Initial all state
+      this.setState(() => {
+        return { fireState: true };
+      });
       let lastPosition = rocketPosition;
       let speed = 10;
       const onRocketMove = () => {
         if (lastPosition < base / 1.5) speed = 20; // Set speed up
-        if (lastPosition < -500) clearInterval(starStarShip);
-        // Stop interval
-        else {
-          lastPosition = lastPosition - speed;
-          this.setState(() => {
-            return { rocketPosition: lastPosition };
-          });
+
+        // ────────────────────────────────────────────────────────────────────────────────
+        // ────────────────────────────────────────────────────────────────────────────────
+
+        //
+        // MOVE UP
+        //
+        if (_control === "up") {
+          if (lastPosition < -700) {
+            clearInterval(starStarShip);
+            this.setState(() => {
+              return { fireState: false };
+            });
+          }
+          // Stop interval
+          else {
+            lastPosition = lastPosition - speed;
+            this.setState(() => {
+              return { rocketPosition: lastPosition };
+            });
+          }
         }
 
-        console.log(lastPosition);
+        // ────────────────────────────────────────────────────────────────────────────────
+        // ────────────────────────────────────────────────────────────────────────────────
+
+        //
+        // MOVE DOWN
+        //
+        if (_control === "down") {
+          if (lastPosition > base - 1) {
+            clearInterval(starStarShip);
+            this.setState(() => {
+              return { fireState: false };
+            });
+          }
+          // Stop interval
+          else {
+            lastPosition = lastPosition + speed;
+            this.setState(() => {
+              return { rocketPosition: lastPosition };
+            });
+          }
+        }
+
+        // ────────────────────────────────────────────────────────────────────────────────
+        // ────────────────────────────────────────────────────────────────────────────────
+
+        //console.log(lastPosition);
       };
 
       // Call interval
       var starStarShip = setInterval(onRocketMove, 10);
     };
 
-    const { rocketPosition } = this.state;
+    const { rocketPosition, fireState } = this.state;
     return (
       <Container>
         <RocketSpace positionY={rocketPosition} middle={middle}>
-          <Rocket />
+          <Rocket fireState={fireState} />
         </RocketSpace>
-        <button onClick={() => moveRocket()}>Hello</button>
-        {console.log("middle : " + middle)}
-        {console.log("base : " + base)}
+        <button onClick={() => moveRocket("up")}>Up</button>
+        <button onClick={() => moveRocket("down")}>Down</button>
       </Container>
     );
   }
@@ -56,8 +106,4 @@ const RocketSpace = styled.div`
   top: ${(props) => props.positionY + "px"};
 `;
 
-const Container = styled.div`
-  position: absolute;
-  height: 100vh;
-  width: 100vw;
-`;
+const Container = styled.div``;
--- client/src/components/rocket/Rocket.js
+++ client/src/components/rocket/Rocket.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import styled from "styled-components";
 import rocketModel from "./assets/rocket.png";
+import fire from "./assets/fire.png";
 
 export default class Rocket extends Component {
   constructor() {
@@ -9,7 +10,22 @@ export default class Rocket extends Component {
   }
 
   render() {
-    return <img src={rocketModel} width="500" height="500" />;
+    const { fireState } = this.props;
+    return (
+      <Position>
+        <img src={rocketModel} width="500" height="500" />
+        <Power src={fire} width="150" height="150" fireState={fireState} />
+      </Position>
+    );
   }
 }
-const Position = styled.div``;
+const Position = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Power = styled.img`
+  transform: rotate(180deg);
+  visibility: ${(props) => (props.fireState ? "visible" : "hidden")};
+`;
