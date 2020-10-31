4dc499adf98ce2d8b8d7c14b95dc64aaf1f2cc07
bluebearrii <thananan.math.acs@gmail.com>
2020-11-01 02:06:23 +0700

    :lipstick: Custom planet

---
+++ client/src/components/earth/Earth.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+
+
+export default class Earth extends Component {
+  render() {
+    return <Container>Earth</Container>;
+  }
+}
+
+const Container = styled.div``;
